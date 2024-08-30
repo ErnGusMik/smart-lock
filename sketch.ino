8ddeaddd43f1b29548e927c9a22e1d5d80e511e2
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-08-31 01:53:56 +0200

    Update sketch.ino
    Small changes, mostly just to find out what was causing the error (turns out it was the Keypad library)

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,7 @@
 // Now turn this trash into treasure!
 
 #include <Servo.h>
-#include <Keypad.h>
+#include <Pin.h>
 
 // Constants
 const int led_pins[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 14 };
@@ -9,19 +9,38 @@ int oldBtnVal = HIGH;
 
 const uint8_t ROWS = 4;
 const uint8_t COLS = 4;
-char keys[ROWS][COLS] = {
+char keys[COLS][ROWS] = {
   { '1', '2', '3', 'A' },
   { '4', '5', '6', 'B' },
   { '7', '8', '9', 'C' },
   { '*', '0', '#', 'D' }
 };
 
-uint8_t colPins[COLS] = { 26, 22, 21, 20 }; // Pins connected to C1, C2, C3, C4
+uint8_t colPins[COLS] = { 20, 21, 22, 26 }; // Pins connected to C1, C2, C3, C4
 uint8_t rowPins[ROWS] = { 16, 17, 18, 19 }; // Pins connected to R1, R2, R3, R4
 
-Keypad keypad = keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+// Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+for (int i = 0; i < 4; i++) {
+  rowPins[i] = Pin(rowPins[i], Pin.OUT)
+  rowPins[i].value(1)
+  colPins[i] = Pin(colPins[i], Pin.IN, Pin.PULL_UP)
+}
+
+void keypad(col, row) {
+  for r in row:
+    r.value(0)
+    result = [col[0].value(), col[1].value(), col[2].value()]
+    if (min(result) == 0):
+      key = key_list[int(row.index(r))][int(result.index(0))]
+      r.value(1)
+      return (key)
+    r.value(1)
+}
+
 
 Servo servo;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -48,7 +67,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500); // this speeds up the simulation (0.5sec)
+  delay(1); // this speeds up the simulation (0.5sec)
 
   // LED strip
   // for (int i = 0; i < 9; i++) {
@@ -79,9 +98,9 @@ void loop() {
   // servo.write(deg)
 
   // Keypad
-  char key = keypad.getKey();
+  // char key = keypad.getKey();
 
-  if (key != NO_KEY) {
-    Serial1.println(key);
-  }
+  // if (key != NO_KEY) {
+  //   Serial1.println(key);
+  // }
 }
