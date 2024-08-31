e28a380bd7c0c6a6f747332ce46d047f41d95886
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-09-01 02:55:08 +0300

    Finished (mostly)

--- sketch.ino
+++ sketch.ino
@@ -18,7 +18,8 @@ char keys[ROWS][COLS] = {
   { '*', '0', '#', 'D' }
 };
 
-
+const String combination = "1234";
+String entered = "";
 
 int oldBtnVal = HIGH;
 
@@ -27,29 +28,29 @@ Servo servo;
 char checkKeypad() {
   int col = -1;
   int row = -1;
+  char character = 'x';
 
   for (int i = 0; i < COLS; i++) {
     if (digitalRead(keypadCols[i]) == LOW) {
-      Serial1.println("smth pressed");
       col = i;
 
       for (int j = 0; j < ROWS; j++) {
         digitalWrite(keypadRows[j], HIGH);
 
         if (digitalRead(keypadCols[i]) == HIGH) {
-          Serial1.println("KEY FOUND!");
           row = j;
+          character = keys[i][j];
         }
 
         digitalWrite(keypadRows[j], LOW);
       }
     }
   }
+  if (col = -1) {
+    return character;
+  }
 
-  Serial1.println(col);
-  Serial1.println(row);
-
-  return '1';
+  return character;
 }
 
 
@@ -59,25 +60,25 @@ void setup() {
   Serial1.println("Hello, Raspberry Pi Pico W!");
 
   // // LED strip
-  // for (int i = 0; i < 9; i++) {
-  //   pinMode(led_pins[i], OUTPUT);
-  // }
-  // pinMode(led_pins[9], OUTPUT);
+  for (int i = 0; i < 9; i++) {
+    pinMode(led_pins[i], OUTPUT);
+  }
+  pinMode(led_pins[9], OUTPUT);
 
   // Button
   pinMode(15, INPUT_PULLUP);
 
   // // Buzzer
-  // pinMode(28, OUTPUT);
+  pinMode(28, OUTPUT);
 
   // Servo
-  // servo.attach(27);
-  // servo.write(0);
+  servo.attach(27);
+  servo.write(0);
 
   // // RGB light
-  // pinMode(11, OUTPUT);
-  // pinMode(12, OUTPUT);
-  // pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 
   for (int i = 0; i < ROWS; i++) {
     pinMode(keypadRows[i], OUTPUT); // PULLUP?
@@ -89,64 +90,80 @@ void setup() {
 }
 
 
+void onSuccess() {
+  tone(28, 1000, 50);
+  servo.write(90);
+  delay(100);
+
+
+  digitalWrite(led_pins[0], HIGH);
+  digitalWrite(led_pins[1], HIGH);
+
+  for (int i = 0; i < 5; i++) {
+    for (int j = 2; j < 10 - i*2; j++) {
+      digitalWrite(led_pins[j], HIGH);
+    }
+
+    analogWrite(11, 255);
+    delay(1000);
+    analogWrite(11, 0);
+    delay(500);
+    for (int j = 2; j < 10; j++) {
+      digitalWrite(led_pins[j], LOW);
+    }
+  }
+
+  digitalWrite(led_pins[0], LOW);
+  digitalWrite(led_pins[1], LOW);
+
+  servo.write(0);
+  tone(28, 500, 25);
+  delay(100);
+  tone(28, 500, 25);
+  analogWrite(11, 0);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500); // this speeds up the simulation (0.5sec)
-
-  // LED strip
-  // for (int i = 0; i < 9; i++) {
-  //   if (digitalRead(led_pins[i]) == HIGH) {
-  //     digitalWrite(led_pins[i], LOW);
-  //     Serial1.println("Setting LOW");
-  //   } else {
-  //     digitalWrite(led_pins[i], HIGH);
-  //     Serial1.println("Setting HIGH");
-  //   }
-  // }
+  delay(50); // this speeds up the simulation (0.5sec)
 
   // Button & buzzer
   const int newBtnVal = digitalRead(15);
 
-  // Buzzer
-  // tone(pin, Hz, ms)
-
-  // Servo
-  // servo.write(deg)
-
   if (newBtnVal != oldBtnVal && newBtnVal == LOW) {
-    tone(28, 1000, 50);
-    // servo.write(90);
-    delay(100);
-
-
-    digitalWrite(led_pins[0], HIGH);
-    digitalWrite(led_pins[1], HIGH);
-
-    for (int i = 0; i < 5; i++) {
-      for (int j = 2; j < 10 - i*2; j++) {
-       digitalWrite(led_pins[j], HIGH);
-      }
-
-      analogWrite(11, 255);
-      delay(1000);
-      analogWrite(11, 0);
-      delay(500);
-      for (int j = 2; j < 10; j++) {
-       digitalWrite(led_pins[j], LOW);
-      }
-    }
-
-    digitalWrite(led_pins[0], LOW);
-    digitalWrite(led_pins[1], LOW);
-
-    // servo.write(0);
-    tone(28, 500, 25);
-    delay(100);
-    tone(28, 500, 25);
-    analogWrite(11, 0);
+    onSuccess();
   }
 
   oldBtnVal = newBtnVal;
 
-  checkKeypad();
+  char press = checkKeypad();
+
+  if (press != 'x') {
+    entered = entered + press;
+    Serial1.println(entered);
+    
+
+    if (entered == combination) {
+      entered = "";
+      for (int j = 0; j < 10; j++) {
+        digitalWrite(led_pins[j], HIGH);
+      }
+      delay(500);
+      onSuccess();
+    } else if (entered.length() == 4 && entered != combination) {
+      entered = "";
+      for (int j = 0; j < 4; j++) {
+        digitalWrite(led_pins[j], HIGH);
+      }
+      delay(500);
+      for (int j = 0; j < 4; j++) {
+        digitalWrite(led_pins[j], LOW);
+      }
+    }
+  }
+
+  for (int j = 0; j < entered.length(); j++) {
+    digitalWrite(led_pins[j], HIGH);
+  }
+
 }
