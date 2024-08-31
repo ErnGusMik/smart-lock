8a9dfd5478efe06599085a651f58f0315e1da773
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2024-08-31 03:16:21 +0200

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,43 +1,10 @@
 // Now turn this trash into treasure!
 
 #include <Servo.h>
-#include <Pin.h>
 
 // Constants
 const int led_pins[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 14 };
 int oldBtnVal = HIGH;
-
-const uint8_t ROWS = 4;
-const uint8_t COLS = 4;
-char keys[COLS][ROWS] = {
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
-};
-
-uint8_t colPins[COLS] = { 20, 21, 22, 26 }; // Pins connected to C1, C2, C3, C4
-uint8_t rowPins[ROWS] = { 16, 17, 18, 19 }; // Pins connected to R1, R2, R3, R4
-
-// Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
-for (int i = 0; i < 4; i++) {
-  rowPins[i] = Pin(rowPins[i], Pin.OUT)
-  rowPins[i].value(1)
-  colPins[i] = Pin(colPins[i], Pin.IN, Pin.PULL_UP)
-}
-
-void keypad(col, row) {
-  for r in row:
-    r.value(0)
-    result = [col[0].value(), col[1].value(), col[2].value()]
-    if (min(result) == 0):
-      key = key_list[int(row.index(r))][int(result.index(0))]
-      r.value(1)
-      return (key)
-    r.value(1)
-}
-
 
 Servo servo;
 
@@ -62,12 +29,17 @@ void setup() {
   // Servo
   servo.attach(27);
   servo.write(0);
+
+  // RGB light
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1); // this speeds up the simulation (0.5sec)
+  delay(500); // this speeds up the simulation (0.5sec)
 
   // LED strip
   // for (int i = 0; i < 9; i++) {
@@ -83,24 +55,55 @@ void loop() {
   // Button & buzzer
   const int newBtnVal = digitalRead(15);
 
-  if (newBtnVal == LOW) {
-    // Serial1.println("Btn pressed!");
-  } else {
-    // Serial1.println("Btn not pressed");
-  }
+  // if (newBtnVal == LOW) {
+  //   // Serial1.println("Btn pressed!");
+  // } else {
+  //   // Serial1.println("Btn not pressed");
+  // }
 
-  oldBtnVal = newBtnVal;
 
   // Buzzer
   // tone(pin, Hz, ms)
 
   // Servo
   // servo.write(deg)
+  
+  // RGB light
+  // analogWrite(11, 255);
+  // analogWrite(12, 0);
+  // analogWrite(13, 0);
 
-  // Keypad
-  // char key = keypad.getKey();
 
-  // if (key != NO_KEY) {
-  //   Serial1.println(key);
-  // }
+
+  if (newBtnVal != oldBtnVal && newBtnVal == LOW) {
+    tone(28, 1000, 500);
+    servo.write(90);
+
+    for (int i = 0; i < 9; i++) {
+      digitalWrite(led_pins[i], HIGH);
+    }
+
+    digitalWrite((led_pins[9], HIGH));
+
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 8 - i*2; j++) {
+       digitalWrite(led_pins[j], HIGH);
+      }
+
+      analogWrite(11, 255);
+      analogWrite(12, 0);
+      analogWrite(13, 0);
+      delay(1000);
+      analogWrite(11, 0);
+    }
+    servo.write(0);
+    tone(28, 500, 250);
+    delay(100);
+    tone(28, 500, 250);
+  }
+
+  oldBtnVal = newBtnVal;
+
+
+
 }
